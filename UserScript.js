fd39b7b365b933f2e6e7dd17d701d61917a21035
Alexander Wolf <fake.wolf@2ch.hk>
2023-11-27 11:56:13 +1000

    Create UserScript.js

---
+++ UserScript.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         YouTube Pause Fix
+// @version      0.2
+// @description  Fix youtube pause on SpaceBar after alt-tab
+// @author       http://github.com/Wolf49406
+// @match        *://www.youtube.com/watch*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function PlayPause() {
+        const video = document.getElementsByClassName("html5-main-video")[0];
+        const movie_player = document.getElementById("movie_player");
+        if (video == undefined || movie_player == undefined) {
+            return;
+        }
+
+        const class_name = movie_player.className;
+        if (class_name == undefined) {
+            return;
+        }
+
+        if (!class_name.includes("ytp-probably-keyboard-focus")) {
+            return;
+        }
+
+        video.click();
+    }
+
+    function IsCommentBlockFocused() {
+        const input_container = document.getElementById("labelAndInputContainer");
+        if (input_container == undefined) {
+            return false;
+        }
+
+        const class_name = input_container.className;
+        if (class_name == undefined) {
+            return false;
+        }
+
+        if (class_name.includes("focused")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function onKeydown(key) {
+        if (key.code != "Space") {
+            return;
+        }
+
+        if (IsCommentBlockFocused()) {
+            return;
+        }
+
+        PlayPause();
+    }
+
+    document.addEventListener('keydown', onKeydown, true);
+})();
